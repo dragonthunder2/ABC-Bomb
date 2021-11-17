7a7af81e2e0f5ff4c699fb5155f6f71994891a44
dragonthunder2 <cuong19052002@gmail.com>
2021-11-17 09:44:57 +0700

    xử lý di chuyển + xử lý va chạm vật cản.

---
+++ src/uet/oop/bomberman/entities/character/Bomber.java
@@ -0,0 +1,174 @@
+package uet.oop.bomberman.entities.character;
+
+import java.util.ArrayList;
+import uet.oop.bomberman.Board;
+import uet.oop.bomberman.Game;
+import uet.oop.bomberman.entities.Entity;
+import uet.oop.bomberman.graphics.Screen;
+import uet.oop.bomberman.graphics.Sprite;
+import uet.oop.bomberman.input.Keyboard;
+
+import java.util.Iterator;
+import java.util.List;
+import uet.oop.bomberman.entities.LayeredEntity;
+import uet.oop.bomberman.level.Coordinates;
+
+public class Bomber extends Character {
+
+    protected Keyboard _input;
+    /**
+     * nếu giá trị này < 0 thì cho phép đặt đối tượng Bomb tiếp theo,
+     * cứ mỗi lần đặt 1 Bomb mới, giá trị này sẽ được reset về 0 và giảm dần trong mỗi lần update()
+     */
+    protected int _timeBetweenPutBombs = 0;
+
+    public Bomber(int x, int y, Board board) {
+        super(x, y, board);
+        _input = _board.getInput();
+        _sprite = Sprite.player_right;
+    }
+
+    @Override
+    public void update() {
+        if (!_alive) {
+            afterKill();
+            return;
+        }
+
+        if (_timeBetweenPutBombs < -7500) _timeBetweenPutBombs = 0;
+        else _timeBetweenPutBombs--;
+
+        animate();
+
+        calculateMove();
+
+    }
+
+    @Override
+    public void render(Screen screen) {
+        calculateXOffset();
+
+        if (_alive)
+            chooseSprite();
+        else
+            _sprite = Sprite.player_dead1;
+
+        screen.renderEntity((int) _x, (int) _y - _sprite.SIZE, this);
+    }
+
+    public void calculateXOffset() {
+        int xScroll = Screen.calculateXOffset(_board, this);
+        Screen.setOffset(xScroll, 0);
+    }
+
+    @Override
+    public void kill() {
+        if (!_alive) return;
+        _alive = false;
+    }
+
+    @Override
+    protected void afterKill() {
+        if (_timeAfter > 0) --_timeAfter;
+        else {
+            _board.endGame();
+        }
+    }
+
+    @Override
+    protected void calculateMove() {
+        // TODO: xử lý nhận tín hiệu điều khiển hướng đi từ _input và gọi move() để thực hiện di chuyển
+        // TODO: nhớ cập nhật lại giá trị cờ _moving khi thay đổi trạng thái di chuyển
+        int xa = 0, ya = 0;
+		if(_input.up) ya--;
+		if(_input.down) ya++;
+		if(_input.left) xa--;
+		if(_input.right) xa++;
+		
+		if(xa != 0 || ya != 0)  {
+			move(xa * Game.getBomberSpeed(), ya * Game.getBomberSpeed());
+			_moving = true;
+		} else {
+			_moving = false;
+		}
+    }
+
+    @Override
+    public boolean canMove(double x, double y) {
+        // TODO: kiểm tra có đối tượng tại vị trí chuẩn bị di chuyển đến và có thể di chuyển tới đó hay không
+       for (int c = 0; c < 4; c++) { //colision detection for each corner of the player
+			double xt = ((_x + x) + c % 2 * 9) / Game.TILES_SIZE; //divide with tiles size to pass to tile coordinate
+			double yt = ((_y + y) + c / 2 * 10 - 13) / Game.TILES_SIZE; //these values are the best from multiple tests
+			
+			Entity a = _board.getEntity(xt, yt, this);
+			
+			if(!a.collide(this))
+				return false;
+		}
+		
+		return true;
+        //return false;
+    }
+
+    @Override
+    public void move(double xa, double ya) {
+        // TODO: sử dụng canMove() để kiểm tra xem có thể di chuyển tới điểm đã tính toán hay không và thực hiện thay đổi tọa độ _x, _y
+        // TODO: nhớ cập nhật giá trị _direction sau khi di chuyển
+        if(xa > 0) _direction = 1;
+		if(xa < 0) _direction = 3;
+		if(ya > 0) _direction = 2;
+		if(ya < 0) _direction = 0;
+		
+		if(canMove(0, ya)) { //separate the moves for the player can slide when is colliding
+			_y += ya;
+		}
+		
+		if(canMove(xa, 0)) {
+			_x += xa;
+		}
+    }
+
+    @Override
+    public boolean collide(Entity e) {
+        // TODO: xử lý va chạm với Flame
+        // TODO: xử lý va chạm với Enemy
+        if( e instanceof LayeredEntity) return(e.collide(this));
+        return true;
+    }
+
+    //sprite
+    private void chooseSprite() {
+        switch (_direction) {
+            case 0:
+                _sprite = Sprite.player_up;
+                if (_moving) {
+                    _sprite = Sprite.movingSprite(Sprite.player_up_1, Sprite.player_up_2, _animate, 20);
+                }
+                break;
+            case 1:
+                _sprite = Sprite.player_right;
+                if (_moving) {
+                    _sprite = Sprite.movingSprite(Sprite.player_right_1, Sprite.player_right_2, _animate, 20);
+                }
+                break;
+            case 2:
+                _sprite = Sprite.player_down;
+                if (_moving) {
+                    _sprite = Sprite.movingSprite(Sprite.player_down_1, Sprite.player_down_2, _animate, 20);
+                }
+                break;
+            case 3:
+                _sprite = Sprite.player_left;
+                if (_moving) {
+                    _sprite = Sprite.movingSprite(Sprite.player_left_1, Sprite.player_left_2, _animate, 20);
+                }
+                break;
+            default:
+                _sprite = Sprite.player_right;
+                if (_moving) {
+                    _sprite = Sprite.movingSprite(Sprite.player_right_1, Sprite.player_right_2, _animate, 20);
+                }
+                break;
+        }
+    }
+}
